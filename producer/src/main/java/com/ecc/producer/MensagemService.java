85d2615225b13f842ad38f2510340fcc62d3834d
EnriqueCarvalho <enriquecarvalho60@gmail.com>
2024-03-24 01:30:42 -0300

    feat: producer funcionando

---
+++ producer/src/main/java/com/ecc/producer/MensagemService.java
@@ -0,0 +1,17 @@
+package com.ecc.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MensagemService {
+    
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("teste", message);
+    }
+}
